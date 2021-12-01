c7cbf8d285cf324924b8a51e915eeb6168c4fd09
techyowl <techyowl@gmail.com>
2021-11-30 23:43:42 -0800

    use env to run mirroring jobs

--- web/jobs/mirror-sync/index.php
+++ web/jobs/mirror-sync/index.php
@@ -1,5 +1,10 @@
 <?php
 include(__DIR__.'/../../helpers/dotenv.php');
 loadEnv(__DIR__ . '/../../config/.env');
+$USE_S3 = get_env('USE_S3');
+$USE_PCLOUD = get_env('USE_PCLOUD');
 $RCLONE_DESTINATION = get_env('RCLONE_DESTINATION');
+if (!$USE_S3 && !$USE_PCLOUD){
+    exit();
+}
 exec(__DIR__ . '/../../bin/rclone --config ' . __DIR__ . '/../../config/rclone.conf --exclude "#recycle/**" --exclude "@eaDir/**" --exclude "@eaDir/"  --exclude "jobs/**/scratch/" --track-renames --ignore-existing --size-only --transfers 2 --checkers 2  --s3-chunk-size 64M -v --log-file=' . realpath(__DIR__ . '/../../logs/mirror-sync-out.txt') .' sync ' . realpath(__DIR__ . '/../../') . ' ' . $RCLONE_DESTINATION);
