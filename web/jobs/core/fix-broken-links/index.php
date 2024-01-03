1a3c8b03f597c15c01448cc59337e2b548b73bda
blaine@wemiller.com <millers@millers>
2024-01-03 07:51:28 +0000

    debug broken link fixer

--- web/jobs/core/fix-broken-links/index.php
+++ web/jobs/core/fix-broken-links/index.php
@@ -49,6 +49,9 @@ foreach(array_chunk($results, 2000) as $key => $chunk) {
             if (
                 $fileMissing && $hostOk
             ) {
+							var_dump([$headers,
+								$fileMissing, $hostOk, $hostHeaders]);
+							continue;
                 $delete_stmt = $conn->prepare("delete FROM media WHERE id = ?");
                 $delete_stmt->execute([$row['id']]);
                 echo PHP_EOL.$delete_stmt->rowCount() . " - Deleted missing remote media url: " . $row['url'];
@@ -62,6 +65,9 @@ foreach(array_chunk($results, 2000) as $key => $chunk) {
                     $hostHeaders = get_headers($protocol."://".$hostname, TRUE);
 
                     if ( (strpos($hostHeaders[0], '200') !== FALSE || strpos($hostHeaders[0], '302') !== FALSE || strpos($hostHeaders[0], '301') !== FALSE ) && (strpos($headers[0], '200') === FALSE || (strpos($head['content-type'], 'image') === FALSE && strpos($head['content-type'], 'video') === FALSE) )) {
+												var_dump([$headers,
+								$fileMissing, $hostOk, $hostHeaders]);
+							continue;
                         $delete_stmt = $conn->prepare("delete FROM media WHERE id = ?");
                         $delete_stmt->execute([$row['id']]);
                         echo PHP_EOL.$delete_stmt->rowCount() . " - Deleted missing remote media thumbnail: " . $row['thumbnail'];
