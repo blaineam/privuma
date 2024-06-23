bd8cf971b7d1002d02c185db9f6fbda338cd9b6b
blaineam <blaineam@users.noreply.github.com>
2024-06-23 08:25:26 +0000

    Fix styling

--- web/jobs/core/db-transfermer/index.php
+++ web/jobs/core/db-transfermer/index.php
@@ -8,6 +8,6 @@ $privuma = privuma::getInstance();
 $conn = $privuma->getPDO();
 
 $blocklist = array_map('strtoupper', json_decode(file_get_contents($privuma->getConfigDirectory() . DIRECTORY_SEPARATOR . 'download-blocklist.json'), true) ?? []);
-if (count($blocklist) > 0 ) {
-    echo PHP_EOL. "Set Blocked column for: " . $conn->query("update media set blocked = case when upper(concat('Album: ', album, '\nFilename: ', filename, '\n', COALESCE(metadata, ''))) REGEXP '(^|\n)(TAGS|TITLE|DESCRIPTION|FILENAME|ALBUM):[^:]*(" . implode('|', $blocklist) . ")[^:]*' then 1 else 0 end;")->rowCount() . " rows";
+if (count($blocklist) > 0) {
+    echo PHP_EOL . 'Set Blocked column for: ' . $conn->query("update media set blocked = case when upper(concat('Album: ', album, '\nFilename: ', filename, '\n', COALESCE(metadata, ''))) REGEXP '(^|\n)(TAGS|TITLE|DESCRIPTION|FILENAME|ALBUM):[^:]*(" . implode('|', $blocklist) . ")[^:]*' then 1 else 0 end;")->rowCount() . ' rows';
 }
