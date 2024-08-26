c0356d728c59b4e4a0310b302fcdfa25d8958803
blaine@wemiller.com <millers@millerpi>
2024-08-25 21:04:11 -0700

    fix: docker bugs and add timeout to pdo

--- web/app/privuma.php
+++ web/app/privuma.php
@@ -76,6 +76,7 @@ class privuma
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_TIMEOUT => 900,
         ];
         try {
             $this->pdo = new PDO($dsn, $user, $pass, $options);
